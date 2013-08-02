6e3ddc55af4eea71edd308ba2d22006b07af6ffd
Victor Moneratto <victor.moneratto@gmail.com>
2013-08-02 00:08:18 -0300

    Added draft of camera tweening movement

--- Game/Game/Components/Entities.cs
+++ Game/Game/Components/Entities.cs
@@ -1,7 +1,9 @@
+using InfiniteIsland.Engine;
 using InfiniteIsland.Engine.Math;
 using InfiniteIsland.Entity;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using IDrawable = InfiniteIsland.Engine.Interface.IDrawable;
 using IUpdateable = InfiniteIsland.Engine.Interface.IUpdateable;
 
@@ -26,11 +28,30 @@ namespace InfiniteIsland.Components
             spriteBatch.End();
         }
 
+        //Factor is the % of 'completition'
+        private float _factor = 1f;
+        //There should be something (a class, maybe?) to update this Tweening.
+        private float? _nextFactor;
+
         public void Update(GameTime gameTime)
         {
+            if (Input.Keyboard.IsKeyTyped(Keys.Tab))
+                _nextFactor = _factor - 1/2f;
+            if (_nextFactor.HasValue)
+            {
+                if (System.Math.Abs(_factor - _nextFactor.Value) > .01f)
+                    _factor += (_nextFactor.Value - _factor) * .05f;
+                else
+                {
+                    _factor = _nextFactor.Value;
+                    _nextFactor = null;
+                }
+            }
+
             Player.Update(gameTime);
             InfiniteIsland.Camera.Viewport.Center =
-                Player.Position.ToPixels() + Vector2.UnitX * .3f * InfiniteIsland.Camera.Viewport.Dimensions;
+                Player.Position.ToPixels() -
+                Vector2.UnitX * .4f *(2f*_factor - 1) * InfiniteIsland.Camera.Viewport.Dimensions;
         }
     }
 }
