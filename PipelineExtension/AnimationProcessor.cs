9b6ba1dfa48b006e4346f25016ed6fef13284767
Victor Moneratto <victor.moneratto@gmail.com>
2015-03-16 23:17:25 -0300

    Files cleanup (builds will go away when dependencies are listed)

--- PipelineExtension/AnimationProcessor.cs
+++ PipelineExtension/AnimationProcessor.cs
@@ -15,30 +15,27 @@ namespace PipelineExtension
             //Load texture
             var textureFileName = input.GetChild("image").Value as string;
 
-            animationContent.Texture = context.BuildAndLoadAsset<TextureContent, TextureContent>(
-                sourceAsset: new ExternalReference<TextureContent>(textureFileName),
-                processorName: "TextureProcessor");
+            animationContent.Texture =
+                context.BuildAndLoadAsset<TextureContent, TextureContent>(
+                    new ExternalReference<TextureContent>(textureFileName), "TextureProcessor");
 
             //Get dimensions and add animations
-            Vector2 dimensions = Vector2.Zero;
+            var dimensions = Vector2.Zero;
             input = input.GetChild("animation");
             animationContent.Animations = new Dictionary<string, Rectangle[]>();
 
-            foreach (Tag animation in input.GetChildren(false))
+            foreach (var animation in input.GetChildren(false))
             {
-                IList<Tag> frames = animation.GetChildren("frame", false);
+                var frames = animation.GetChildren("frame", false);
                 var rectangles = new Rectangle[frames.Count];
 
-                for (int i = 0; i < rectangles.Length; ++i)
+                for (var i = 0; i < rectangles.Length; ++i)
                 {
-                    rectangles[i] = new Rectangle(
-                        x: (int)frames[i].Attributes["left"],
-                        y: (int)frames[i].Attributes["top"],
-                        width: (int)frames[i].Attributes["width"],
-                        height: (int)frames[i].Attributes["height"]);
+                    rectangles[i] = new Rectangle((int) frames[i].Attributes["left"], (int) frames[i].Attributes["top"],
+                        (int) frames[i].Attributes["width"], (int) frames[i].Attributes["height"]);
 
-                    dimensions.X = MathHelper.Max(dimensions.X, (int)frames[i].Attributes["width"]);
-                    dimensions.Y = MathHelper.Max(dimensions.Y, (int)frames[i].Attributes["height"]);
+                    dimensions.X = MathHelper.Max(dimensions.X, (int) frames[i].Attributes["width"]);
+                    dimensions.Y = MathHelper.Max(dimensions.Y, (int) frames[i].Attributes["height"]);
                 }
 
                 animationContent.Animations[animation.Name] = rectangles;
